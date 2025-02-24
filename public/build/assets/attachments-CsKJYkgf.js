0e4b1daff9dd29b9582e426e6ac24acdd4144ceb
lymbf <paw.czekaj@gmail.com>
2025-02-24 23:08:57 +0100

    removed build

--- public/build/assets/attachments-CsKJYkgf.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{fetch("/api/attachments").then(t=>t.json()).then(t=>{console.log("Attachments data:",t);const e=document.getElementById("attachments");e&&(t.length?e.innerHTML=t.map(n=>`<p>${n.original_name}</p>`).join(""):e.innerHTML="<p>Brak załączników</p>")}).catch(t=>console.error("Error fetching attachments:",t))});
